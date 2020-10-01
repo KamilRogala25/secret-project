5f45eb6e40a182ae476096adc3a8d316f8d49f43
Kamil Rogala <rogalak25@gmail.com>
2020-10-01 20:44:51 +0200

    "Fixed not refreshing time"

--- src/main/java/com/example/popehour/model/HourComparator.java
+++ src/main/java/com/example/popehour/model/HourComparator.java
@@ -1,49 +1,58 @@
 package com.example.popehour.model;
 
 import lombok.Getter;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.time.LocalTime;
+import java.util.Timer;
+import java.util.TimerTask;
 
-
+@EnableScheduling
 public abstract class HourComparator {
 
-    private static String actualTime = LocalTime.now().getHour() + ":" + LocalTime.now().getMinute();
+    private static String actualTime;
 
     private static final String targetTime = "21:37";
 
-    public static boolean compareHours(){
-        if (actualTime.equalsIgnoreCase(targetTime)){
+    public static boolean compareHours() {
+        if (actualTime.equalsIgnoreCase(targetTime)) {
             return true;
-        }
-        else return false;
+        } else return false;
     }
 
-    public static String howMuchLeft(){
+    public static String howMuchLeft() {
+        updateActualTime();
         String[] actual = HourComparator.getActualTime().split(":");
         String[] target = HourComparator.getTargetTime().split(":");
         int hoursDiff = Integer.parseInt(target[0]) - Integer.parseInt(actual[0]);
         int minutesDiff = Integer.parseInt(target[1]) - Integer.parseInt(actual[1]);
-        if (hoursDiff >0 && minutesDiff >0){
+        if (hoursDiff > 0 && minutesDiff > 0) {
             return "Do godziny papieskiej zostało " + hoursDiff + " godzin i " + minutesDiff + " minut.";
         }
-        if (hoursDiff <0 && minutesDiff >0){
+        if (hoursDiff < 0 && minutesDiff > 0) {
             hoursDiff *= -1;
             return "Do godziny papieskiej zostało " + hoursDiff + " godzin i " + minutesDiff + " minut.";
         }
-        if (hoursDiff >0 && minutesDiff <0){
-            minutesDiff = (minutesDiff*-1) +37;
+        if (hoursDiff > 0 && minutesDiff < 0) {
+            minutesDiff = (minutesDiff * -1) + 37;
             return "Do godziny papieskiej zostało " + hoursDiff + " godzin i " + minutesDiff + " minut.";
         }
-        if (hoursDiff <0 && minutesDiff <0){
+        if (hoursDiff < 0 && minutesDiff < 0) {
             hoursDiff *= -1;
-            minutesDiff = (minutesDiff*-1) +37;
+            minutesDiff = (minutesDiff * -1) + 37;
             return "Do godziny papieskiej zostało " + hoursDiff + " godzin i " + minutesDiff + " minut.";
         }
         return "0";
     }
-
     public static String getActualTime() {
+        actualTime = LocalTime.now().getHour() + ":" + LocalTime.now().getMinute();
         return actualTime;
+    }
+
+    @Scheduled(fixedRateString = "1000")
+    public static void updateActualTime(){
+        actualTime =getActualTime();
     }
 
     public static String getTargetTime() {
