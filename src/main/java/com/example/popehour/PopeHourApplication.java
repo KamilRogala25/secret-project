4e5b9ac0ff82156cac756f30c8f4dfa56f2d9407
Kamil Rogala <rogalak25@gmail.com>
2020-10-01 11:38:31 +0200

    "First blank version. Ready for coding"

---
+++ src/main/java/com/example/popehour/PopeHourApplication.java
@@ -0,0 +1,13 @@
+package com.example.popehour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopeHourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopeHourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/popehour/PopeHourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.popehour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopeHourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
